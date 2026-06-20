865eb7e5a86d43c1964fccf3e1ad085365af82dc
obeconene381-cmyk <obeconene381@gmail.com>
2026-06-20 20:13:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-python3 monitor.py
+
+# نشغلوا السكربت تاع المراقبة في الخلفية باستخدام &
+python3 monitor.py &
+
+# نشغلوا الـ Xray في الواجهة الأمامية (بدون &)
+# هكذا السكربت ماراحش يكمل (exit) حتى يطفى الـ Xray
+./xray run -config config.json
